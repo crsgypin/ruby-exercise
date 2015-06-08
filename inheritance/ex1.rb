7007bf169f5ba5a333bff0fb5f8d3fee5c29d5e9
crsgypin <crsgypin@gmail.com>
2015-06-08 22:09:23 +0800

    inheritance ex1

---
+++ inheritance/ex1.rb
@@ -0,0 +1,29 @@
+
+
+# Create a superclass called Vehicle for your MyCar class to inherit from and move the behavior that isn't specific to the MyCar class to the superclass. Create a constant in your MyCar class that stores information about the vehicle that makes it different from other types of Vehicles.
+
+#Then create a new class called MyTruck that inherits from your superclass that also has a constant defined that separates it from the MyCar class in some way.
+
+class Vehicle
+	def self.gas_mileage(gallons, miles)
+		puts "#{miles/gallons} miles per gallon of gas"
+	end
+end
+
+class MyCar < Vehicle
+  NUMBER_OF_DOORS = 4
+
+  def doors
+    NUMBER_OF_DOORS
+  end
+
+end
+
+class MyTruck < Vehicle
+  NUMBER_OF_DOORS = 2
+end
+
+
+car = MyCar.new
+
+puts car.doors
