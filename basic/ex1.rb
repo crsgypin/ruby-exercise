82d5b4499ce9baf506482f55dad96f00eae44af2
crsgypin <crsgypin@gmail.com>
2015-06-08 20:28:51 +0800

    ex1.rb

---
+++ basic/ex1.rb
@@ -0,0 +1,10 @@
+
+#Add two strings together that, when concatenated, return your first and last name as your full name in one string.
+
+def concat(name1, name2)
+	name1 + ' ' + name2
+
+end
+
+
+puts concat("John","Doe")
