50e26d53e0c6c5ccc38aab983fea27754c9bc109
crsgypin <crsgypin@gmail.com>
2015-06-08 20:29:41 +0800

    ex3.rb

---
+++ basic/ex3.rb
@@ -0,0 +1,16 @@
+
+#Write a program that uses a hash to store a list of movie titles with the year they came out. Then use the puts command to make your program print out the year of each movie to the screen. The output for your program should look something like this.
+
+movies = { :movie1 => 1975,
+			:movie2 => 2004,
+			:movie3 => 2013,
+			:movie4 => 2001,
+			:movie5 => 1981
+		}
+
+movies.each do |key,value|
+	puts value
+end
+
+
+
