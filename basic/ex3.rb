eb091dfabf18dd8cf744f26b9468b0997331e709
crsgypin <crsgypin@gmail.com>
2015-06-08 20:31:04 +0800

    ex4, updaed in ex3.rb

--- basic/ex3.rb
+++ basic/ex3.rb
@@ -14,3 +14,11 @@ end
 
 
 
+#Use the dates from the previous example and store them in an array. Then make your program output the same thing as exercise 3.
+
+dates = [1975, 2004, 2013, 2001, 1981]
+
+dates.each do |value|
+	puts value
+end
+
